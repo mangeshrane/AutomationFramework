939a2b25fb0a3d741c0477b93a4de790c0ae2718
Mangesh Rane <ranemangesh555@gmail.com>
2018-10-02 18:51:43 +0100

    Day-1 progress

--- src/main/java/utillities/DriverFactory.java
+++ src/main/java/utillities/DriverFactory.java
@@ -1,5 +1,11 @@
 package utillities;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverFactory {
 	
+	ThreadLocal<Object> driver = new ThreadLocal<Object>();
+	{
+		driver.set(new ChromeDriver());
+	}
 }
